2ea948eafddc9f9f1411f833c0b549bfe7e1a908
Shail Shah <shail.shah0703@gmail.com>
2021-07-19 16:25:52 +0530

    controller added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const userRouter=require('./routes/user.routes');
 
 app.use(helmet());
+app.use('/api/v1/familytree',userRouter);
 module.exports = app;
